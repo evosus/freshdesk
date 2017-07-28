1cecc8e9074820c9361b239d5fa6069eb84a108c
Robert Norton <spiff.spuzzum@gmail.com>
2017-07-27 17:39:43 -0700

    random selection of tip video

---
+++ enterprise/enterprise-main.js
@@ -0,0 +1,88 @@
+/* Randomly pick a tip to load*/
+jQuery_2_1_1( document ).ready(function() {
+
+	var selection = Math.round(23 * (Math.random());
+	console.log(selection);
+
+	switch(selection) {
+		case 1:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/bAh18X4qFx8?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 2:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/Kk6l-9m78HM?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 3:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/zXvVa7FtObU?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 4:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/8cByE7Vm8Gk?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 5:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/i1gdfCYFPxQ?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 6:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/wuwRocKJ82s?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 7:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/qqZln90k6kw?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 8:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/Iwxnyq1qVm4?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 9:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/GKiaZp-V5oo?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 10:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/beZGDJi_RuI?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 11:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/VsVXjG82m_0?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 12:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/tDdcs91EY-0?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 13:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/EMXN1g6G95s?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 14:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/0w3WBa3xgRw?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 15:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/O2om16x_Q3E?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 16:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/RGfmmbzjgkI?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 17:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/HauaYns3b7w?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 18:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/KDWsQ_nd-Xo?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 19:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/fE6aVN7hZ_g?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 20:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/Cnb-dZoEf-w?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 21:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/NCzK0EEnkTU?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 22:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/r_RBlqsA5hY?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+		case 23:
+			document.getElementById("videoTip").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/ogYFRkzMHHY?list=PLGNyxm4TBvf801-j-KzI_T_oDaj7TH8T8" frameborder="0" allowfullscreen></iframe>';
+			break;
+	}
+});
+
+
+
+
+
+
+
+
+
+
