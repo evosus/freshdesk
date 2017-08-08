a03b27e75d234ca4da6ec768ebc5c045848bd853
Robert Norton <spiff.spuzzum@gmail.com>
2017-08-08 15:48:14 -0700

    evopro mockup

--- global-main.js
+++ global-main.js
@@ -5,11 +5,18 @@ jQuery_2_1_1( document ).ready(function() {
 		jQuery_2_1_1('#login-form-new').addClass("hide");
 		jQuery_2_1_1('#forgot-form-new').removeClass("hide");
 	});
-	jQuery_2_1_1('.forgot-form-submit .control-group [type="button"]').click(function() {
+	
+  jQuery_2_1_1('.forgot-form-submit .control-group [type="button"]').click(function() {
 		console.log("test");
 		jQuery_2_1_1('#login-form-new').removeClass("hide");
 		jQuery_2_1_1('#forgot-form-new').addClass("hide");
 	});
+
+  // Initialize collapse button
+  jQuery_2_1_1(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+
 /* lazy load of cards */
   var options = [
      {selector: '#jsSectionA1', offset: 0, callback: function(el) {
