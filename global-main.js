845b07e7e4678e7c367e214773e980e635f0703f
Robert Norton <spiff.spuzzum@gmail.com>
2017-08-08 17:16:10 -0700

    evopro mockup

--- global-main.js
+++ global-main.js
@@ -1,33 +1,19 @@
-/* Fix forgot password form*/
-jQuery_2_1_1( document ).ready(function() {
-	jQuery_2_1_1('#support-search-input').attr("placeholder", "Search all products...");
-	jQuery_2_1_1('#forgot_password').click(function() {
-		jQuery_2_1_1('#login-form-new').addClass("hide");
-		jQuery_2_1_1('#forgot-form-new').removeClass("hide");
+( document ).ready(function($) {
+	// replace search text
+  $('#support-search-input').attr("placeholder", "Search all products...");
+	
+  // login page button fix 
+  $('#forgot_password').click(function() {
+		$('#login-form-new').addClass("hide");
+		$('#forgot-form-new').removeClass("hide");
 	});
 	
-  jQuery_2_1_1('.forgot-form-submit .control-group [type="button"]').click(function() {
-		console.log("test");
-		jQuery_2_1_1('#login-form-new').removeClass("hide");
-		jQuery_2_1_1('#forgot-form-new').addClass("hide");
+  $('.forgot-form-submit .control-group [type="button"]').click(function() {
+		$('#login-form-new').removeClass("hide");
+		$('#forgot-form-new').addClass("hide");
 	});
 
-/* lazy load of cards */
-  var options = [
-     {selector: '#jsSectionA1', offset: 0, callback: function(el) {
-        Materialize.fadeInImage(jQuery_2_1_1(el));
-        console.log("tripped one");
-      } },
-    {selector: '#jsSectionA2', offset: 50, callback: function(el) {
-        Materialize.showStaggeredList(jQuery_2_1_1(el));
-        console.log("tripped two");
-    } }
-    ];
-    Materialize.scrollFire(options);
-});
-
-$(document).ready (function (){
-   // Initialize collapse button
+  //SIDE MENU
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
