b1da6227c97f8411797b6f76904aafdf1b680eb9
Robert Norton <spiff.spuzzum@gmail.com>
2017-10-02 11:57:37 -0700

    added jquery version to console log
    callback isn't working, so testing to make sure callback isn't being run
    outside no-conflict

--- materialize.noconflict.js
+++ materialize.noconflict.js
@@ -47,8 +47,8 @@ function _classCallCheck(t,e){if(!(t instanceof e))throw new TypeError("Cannot c
 	    startingTop: '10%', // Starting top style attribute
 	    endingTop: '30%', // Ending top style attribute
 	    complete: function() { 
-	    	console.log("closing");
-	    	$('.youtube-iframe').each(function(index) {
+	    	console.log("closing" + jQuery.fn.jquery);
+	    	jQuery('.youtube-iframe').each(function(index) {
         		$(this).attr('src', $(this).attr('src'));
         		return false;
       			});
