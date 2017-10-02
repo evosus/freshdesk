dd82aee48d8a626767a75ad86983798e0edaf569
Robert Norton <spiff.spuzzum@gmail.com>
2017-10-02 15:42:42 -0700

    there are a lot of ways to try to solve this
    I've tried at least 5 at this point

--- materialize.noconflict.js
+++ materialize.noconflict.js
@@ -47,10 +47,12 @@ function _classCallCheck(t,e){if(!(t instanceof e))throw new TypeError("Cannot c
 	    startingTop: '10%', // Starting top style attribute
 	    endingTop: '30%', // Ending top style attribute
 	    complete: function() { 
-	    	var ytIframe = document.querySelector( 'iframe');
-	    	console.log("iframe = " + ytIframe);
-            var iframeSrc = ytIframe.src;
-            ytIframe.src = iframeSrc;
+    	  var iframes = document.querySelectorAll( 'iframe'), i;
+                
+	        for (i=0; i < iframes.length; i++){
+	          var iframeSrc = iframes[i].src;
+	          iframes[i].src=iframeSrc
+	        }
 	    	/*
 	    	jQuery_3_2_1('.youtube-iframe').each(function(index) {
         		$(this).attr('src', $(this).attr('src'));
