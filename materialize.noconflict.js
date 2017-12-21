e80beb52e309acd896419217d594be9b1d437f65
Robert Norton <spiff.spuzzum@gmail.com>
2017-12-20 17:07:01 -0800

    ES-409
    ES-409

--- materialize.noconflict.js
+++ materialize.noconflict.js
@@ -84,6 +84,7 @@ function _classCallCheck(t,e){if(!(t instanceof e))throw new TypeError("Cannot c
     //Fix to new ticket form
     $('#helpdesk_ticket_group_id').addClass('hide');
   	$('#helpdesk_ticket_group_id').prop('selectedIndex', 1);
+  	$('#helpdesk_ticket_group_id>option:nth-child(1)').attr('selected', true);
   	$('label[for="helpdesk_ticket_group"]').addClass("hide");
 	
 
